4a02b797c2f0dd8df0d9f2a412922bee654233d7
umahayu <umahayusetyagita@gmail.com>
2023-01-04 13:16:36 +0700

    Menambahkan Button

--- app/src/main/java/com/zubet/myapplication/MainActivity.kt
+++ app/src/main/java/com/zubet/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.zubet.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnnSatu : Button = findViewById(R.id.btn1)
+        btnnSatu. setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
