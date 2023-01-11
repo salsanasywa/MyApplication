e2e5390785eeca417640a962a076a0a79a16c8af
salsanasywa <salsanasywa3@gmail.com>
2023-01-11 10:15:44 +0700

    Melanjutkan tampilan presensi

--- app/src/main/java/com/zubet/myapplication/LogIn.kt
+++ app/src/main/java/com/zubet/myapplication/LogIn.kt
@@ -1,11 +1,19 @@
 package com.zubet.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
     }
+
+    fun masuk (view: View){
+        val pindah = Intent (this, Opsi::class.java)
+        startActivity(pindah)
+    }
+
 }
--- app/src/main/java/com/zubet/myapplication/MainActivity.kt
+++ app/src/main/java/com/zubet/myapplication/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.zubet.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+abstract class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent (this,LogIn::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
     }
 }
